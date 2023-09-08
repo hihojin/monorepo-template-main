269603f88fdc452a0d357f2a1dfb706a430fe9a1
Hyojin Kwak <hyojinkwak@Hyojins-MacBook-Pro.local>
2023-09-07 20:49:27 -0700

    refactored rectanlge class and added shape interface

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,18 +5,33 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    '''interface that rectanlge implements'''
+    @abstractmethod
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        pass
+    
+    @abstractmethod
+    def area(self):
+        pass 
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self._width = width # private variable
+        self._height = height # private variable
+
+    def set_values(self, x, y):
+        self._width = x
+        self._height = y
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(2, 4)
 
     # Call a member function
     rect.set_values(3, 4)
