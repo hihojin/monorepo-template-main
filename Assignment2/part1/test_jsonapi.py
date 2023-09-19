d2fda26ddf1f34b24c07ada63e73259ba0c97fb3
Hyojin Kwak <hyojinkwak@Hyojins-MBP.lan>
2023-09-18 18:46:59 -0700

    pytest

--- Assignment2/part1/test_jsonapi.py
+++ Assignment2/part1/test_jsonapi.py
@@ -1,22 +1,24 @@
 import jsonapi
 
-json_data = jsonapi.dumps(complex(1, 2))
-print(json_data)
-decoded = jsonapi.loads(json_data)
-print(decoded)
+'''
+running pytest
+'''
 
-json_data = jsonapi.dumps(range(1, 10, 3))
-print(json_data)
-decoded = jsonapi.loads(json_data)
-print(decoded)
+def test_encode_decode_complex():
+    json_data = jsonapi.dumps(complex(1, 2))
+    assert json_data == '{"real": 1.0, "imag": 2.0, "__extended_json_type__": "complex"}'
+    decoded = jsonapi.loads(json_data)
+    assert decoded == (1+2j)
 
-json_data = jsonapi.dumps(range(1, 10))
-print(json_data)
-decoded = jsonapi.loads(json_data)
-print(decoded)
+def test_encode_decode_range():
+    json_data = jsonapi.dumps(range(1, 10, 3))
+    assert json_data == '{"start": 1, "stop": 10, "step": 3, "__extended_json_type__": "range"}'
+    decoded = jsonapi.loads(json_data)
+    assert decoded == range(1, 10, 3)
 
-json_data = jsonapi.dumps({73: False})
-print(json_data)
-decoded = jsonapi.loads(json_data)
-print(decoded)
+def test_encode_decode_json_friendly():
+    json_data = jsonapi.dumps({73: False})
+    assert json_data == '{"73": false}'
+    decoded = jsonapi.loads(json_data)
+    assert decoded == {'73': False}
 
